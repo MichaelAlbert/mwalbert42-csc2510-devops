17e9f7557d9eb414db3d4ad6e2e4e66fb527dfce
Michael Albert <michaelalbert.fhs@gmail.com>
2024-11-04 23:17:00 +0000

    added bug fix

--- poetry/dailygrind.java
+++ poetry/dailygrind.java
@@ -10,7 +10,12 @@ public static final void main(String[] args) {
 			
 		Date now = new Date();   
 		its_time_to_go_home = now.getHours() > 17 && now.getMinutes() > 30;    	
-			if (its_time_to_go_home) {             break;       }
+			if (its_time_to_go_home) {            
+				try {Thread.sleep(60000); } 
+				catch (InterruptedException e) {  	// ignore 
+					 }
+
+				break;       }
 
 }
 }
